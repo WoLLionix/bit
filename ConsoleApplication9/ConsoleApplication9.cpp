c05c845b13a8d4f352a9dab73b2ad817b3f71cdb
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-18 19:45:33 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,72 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    if (n >= 128)
+    {
+        std::cout << 1;
+        n -= 128;
+    }
+    else
+        std::cout << 0;
+
+    if (n >= 64)
+    {
+        std::cout << 1;
+        n -= 64;
+    }
+    else
+        std::cout << 0;
+
+    if (n >= 32)
+    {
+        std::cout << 1;
+        n -= 32;
+    }
+    else
+        std::cout << 0;
+
+    if (n >= 16)
+    {
+        std::cout << 1;
+        n -= 16;
+    }
+    else
+        std::cout << 0;
+
+    if (n >= 8)
+    {
+        std::cout << 1;
+        n -= 8;
+    }
+    else
+        std::cout << 0;
+
+    if (n >= 4)
+    {
+        std::cout << 1;
+        n -= 4;
+    }
+    else
+        std::cout << 0;
+
+    if (n >= 2)
+    {
+        std::cout << 1;
+        n -= 2;
+    }
+    else
+        std::cout << 0;
+
+    if (n >= 1)
+    {
+        std::cout << 1;
+        n -= 1;
+    }
+    else
+        std::cout << 0;
+}
